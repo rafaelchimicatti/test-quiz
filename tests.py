eac51b99f29e4b6af0ee9a67ba4b5252022f0d20
rafaelchimicatti <rafaelchimicatti@hotmail.com>
2026-03-26 19:15:06 -0300

    Commit 2: Creating 10 unit tests.

--- tests.py
+++ tests.py
@@ -34,3 +34,99 @@ def test_create_choice():
     assert len(question.choices) == 1
     assert choice.text == 'a'
     assert not choice.is_correct
+
+from model import Question
+
+
+def test_add_multiple_choices():
+    question = Question(title='q1')
+    question.add_choice('a')
+    question.add_choice('b')
+
+    assert len(question.choices) == 2
+    assert question.choices[0].id == 1
+    assert question.choices[1].id == 2
+
+
+def test_remove_choice_by_id():
+    question = Question(title='q1')
+    c1 = question.add_choice('a')
+    c2 = question.add_choice('b')
+
+    question.remove_choice_by_id(c1.id)
+
+    assert len(question.choices) == 1
+    assert question.choices[0].id == c2.id
+
+
+def test_remove_choice_invalid_id():
+    question = Question(title='q1')
+    question.add_choice('a')
+
+    with pytest.raises(Exception):
+        question.remove_choice_by_id(999)
+
+
+def test_remove_all_choices():
+    question = Question(title='q1')
+    question.add_choice('a')
+    question.add_choice('b')
+
+    question.remove_all_choices()
+
+    assert len(question.choices) == 0
+
+
+def test_set_correct_choices():
+    question = Question(title='q1')
+    c1 = question.add_choice('a')
+    c2 = question.add_choice('b')
+
+    question.set_correct_choices([c1.id])
+
+    assert question.choices[0].is_correct
+    assert not question.choices[1].is_correct
+
+
+def test_set_correct_choices_invalid_id():
+    question = Question(title='q1')
+    question.add_choice('a')
+
+    with pytest.raises(Exception):
+        question.set_correct_choices([999])
+
+
+def test_correct_selected_choices_valid():
+    question = Question(title='q1')
+    c1 = question.add_choice('a', True)
+
+    result = question.correct_selected_choices([c1.id])
+
+    assert result == [c1.id]
+
+
+def test_correct_selected_choices_exceeds_limit():
+    question = Question(title='q1', max_selections=1)
+    c1 = question.add_choice('a', True)
+    c2 = question.add_choice('b', True)
+
+    with pytest.raises(Exception):
+        question.correct_selected_choices([c1.id, c2.id])
+
+
+def test_choice_text_validation():
+    question = Question(title='q1')
+
+    with pytest.raises(Exception):
+        question.add_choice('')
+
+    with pytest.raises(Exception):
+        question.add_choice('a' * 101)
+
+
+def test_points_out_of_range():
+    with pytest.raises(Exception):
+        Question(title='q1', points=0)
+
+    with pytest.raises(Exception):
+        Question(title='q1', points=101)
